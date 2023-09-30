a11c43f445f8b81ab63d6054293b4fbb2cae6d68
Priyashalgar24 <141939915+Priyashalgar24@users.noreply.github.com>
2023-09-30 20:20:03 +0530

    timer.js

--- timer.js
+++ timer.js
@@ -1,23 +1,25 @@
-//refrance active time container
+//reference to active time container
 const activeTimerContainer = document.getElementById("dispalyCurrentTime");
 
-// refrance set button
+// reference to set button
 const setButton = document.getElementById("setTimeButton");
 
 const removeNoContainerDiv = document.getElementById("notext");
 
 //addevent
  setButton.addEventListener("click" , () => {
+   // made button clickable
     const hours = parseInt(document.getElementById("hours").value) || 0;
     const min = parseInt(document.getElementById("minutes").value) || 0;
-    const sec = parseInt(document.getElementById("Secound").value) || 0;
+    const sec = parseInt(document.getElementById("seconds").value) || 0;
     
-    const totalSecound = (hours*3600) + (min*60) + sec ;
-    if (totalSecound > 0){
+    const totalSeconds = (hours*3600) + (min*60) + sec ;
+    if (totalSeconds > 0){
       removeNoContainerDiv.remove();
-      createDiv(totalSecound);
+      createDiv(totalSeconds);
     }else {
       alert("Please enter a valid time.");
+      //setting up alert here;
     }
  });
 
@@ -49,6 +51,7 @@ const removeNoContainerDiv = document.getElementById("notext");
       totalsec--;
       if (totalsec <= 0){
          clearInterval(timerInterval);
+         // using clearinterval method when the timer reaches zero.
          timer.innerText = "Time is up!";
         timer.id="update" 
          timeLeft.style.display="none";
@@ -56,7 +59,7 @@ const removeNoContainerDiv = document.getElementById("notext");
          deleteButton.innerText="STOP!";
          deleteButton.style.backgroundColor="#34344A";
          deleteButton.style.color="white";
-
+         
          playAudioAlert();
       }else{
          timer.textContent = formatTime(totalsec);
@@ -66,14 +69,14 @@ const removeNoContainerDiv = document.getElementById("notext");
    deleteButton.addEventListener("click" , () => {
       timerDiv.remove();
    })
-
+   // using timerinterval method to decrement the timer every second
    let timerInterval = setInterval(updateTimerDisplay, 1000);
 
    timerDiv.append(timeLeft,  timer  , deleteButton);
     
    activeTimerContainer.appendChild(timerDiv);
  }
-
+ //plays audio when the setted timer or alarm is upto the time
  function playAudioAlert() {
    const audio = new Audio('./abc.mp3');
    audio.play();
